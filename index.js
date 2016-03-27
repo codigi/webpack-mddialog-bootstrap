f11c11d64235d83d521e5a4fba01141079f0c1de
Atanas Palavrov <palavrov@gmail.com>
2016-03-27 03:35:14 +0200

    Bootstrap to own div elemnt.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var angularMaterial = require("webpack-angular-material");
 var angularModule = angular.module("$mdDialogBootstrap",["ngMaterial"]);
-var $injector = angular.bootstrap(document.body, ["$mdDialogBootstrap"]);
+var div = document.createElement("div");
+document.body.appendChild(div);
+var $injector = angular.bootstrap(div, ["$mdDialogBootstrap"]);
 var $mdDialog = $injector.get("$mdDialog");
 
 module.exports = $mdDialog;
